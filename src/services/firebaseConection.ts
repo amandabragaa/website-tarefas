1968ab2f3174d6d197fef4ab715a6f13682fd34b
Amanda Braga <amandabraga1998@gmail.com>
2026-06-05 16:38:13 -0300

    fix: removendo cred

--- src/services/firebaseConection.ts
+++ src/services/firebaseConection.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBq2_f3O6YsB7Xr4uRT-AL6QULKpM25f3Y",
-  authDomain: "tarefasplus-c7f2e.firebaseapp.com",
-  projectId: "tarefasplus-c7f2e",
-  storageBucket: "tarefasplus-c7f2e.firebasestorage.app",
-  messagingSenderId: "919945025537",
-  appId: "1:919945025537:web:754daabbfb2bb9500f8800",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
